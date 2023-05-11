3bbf5f7016dcf2222ff9bf727f6bf4e6cb9f52fc
gourav <gt9675tiwari@gmail.com>
2023-05-11 19:40:29 +0530

    basic page structure

--- frontened/src/App.tsx
+++ frontened/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-6xl font-bold underline">
-      Hello world!
-    </h1>
+      <Home/>
     </div>
   );
 }
---
+++ frontened/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Allroutes } from '../allRoutes/Allroutes'
+
+export const Home = () => {
+  return (
+    <Allroutes/>
+  )
+}
